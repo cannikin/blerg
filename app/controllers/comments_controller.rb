dfb89ff738c93eb8f08dc2768eb10503e493e110
Rob Cameron <cannikinn@gmail.com>
2016-03-04 16:11:16 -0800

    Can create new comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,64 +1,40 @@
 class CommentsController < ApplicationController
+
   before_action :set_post
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
-
-  # GET /comments
-  def index
-    @comments = Comment.all
-  end
-
-  # GET /comments/1
-  def show
-  end
-
-  # GET /comments/new
-  def new
-    @comment = Comment.new
-  end
-
-  # GET /comments/1/edit
-  def edit
-  end
+  before_action :set_comment, only: [:destroy]
 
   # POST /comments
   def create
     @comment = Comment.new(comment_params)
+    @comment.post = @post
 
-    if @comment.save
-      redirect_to [@post, @comment], notice: 'Comment was successfully created.'
+    if @comment.save!
+      redirect_to @post, notice: 'Your comment was added!'
     else
-      render :new
-    end
-  end
-
-  # PATCH/PUT /comments/1
-  def update
-    if @comment.update(comment_params)
-      redirect_to [@post, @comment], notice: 'Comment was successfully updated.'
-    else
-      render :edit
+      render @post
     end
   end
 
   # DELETE /comments/1
   def destroy
     @comment.destroy
-    redirect_to post_comments_url(@post), notice: 'Comment was successfully destroyed.'
+    redirect_to post_url(@post), notice: 'Comment was successfully destroyed.'
   end
 
-  private
+private
 
-    def set_post
-      @post = Post.find(params[:post_id])
-    end
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
 
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def comment_params
-      params.require(:comment).permit(:post_id, :name, :email, :body)
-    end
+  # Only allow a trusted parameter "white list" through.
+  def comment_params
+    params.require(:comment).permit(:post_id, :name, :email, :body)
+  end
+
 end
