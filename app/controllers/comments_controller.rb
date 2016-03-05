1d9c705c618a6f3e606ae2139001f8691b797cd6
Rob Cameron <cannikinn@gmail.com>
2016-03-04 19:51:43 -0800

    Removes scaffolding comments from controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,6 @@ class CommentsController < ApplicationController
   before_action :set_post
   before_action :set_comment, only: [:destroy]
 
-  # POST /comments
   def create
     @comment = Comment.new(comment_params)
     @comment.post = @post
@@ -16,26 +15,21 @@ class CommentsController < ApplicationController
     end
   end
 
-  # DELETE /comments/1
   def destroy
     @comment.destroy
     redirect_to post_url(@post), notice: 'Comment was successfully destroyed.'
   end
 
-private
-
-  def set_post
+  private def set_post
     @post = Post.find(params[:post_id])
   end
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_comment
+  private def set_comment
     @comment = Comment.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
-  def comment_params
-    params.require(:comment).permit(:post_id, :name, :email, :body)
+  private def comment_params
+    params.require(:comment).permit(:name, :email, :body)
   end
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,26 +3,21 @@ class PostsController < ApplicationController
   before_action :require_session, :except => [:index, :show]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  # GET /posts
   def index
     @posts = Post.includes(:comments).order(:created_at => :desc)
   end
 
-  # GET /posts/1
   def show
     @new_comment = Comment.new(:post => @post)
   end
 
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
   def edit
   end
 
-  # POST /posts
   def create
     @post = Post.new(post_params)
     @post.user = current_user
@@ -34,7 +29,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /posts/1
   def update
     if @post.update(post_params)
       redirect_to @post, notice: 'Post was successfully updated.'
@@ -43,7 +37,6 @@ class PostsController < ApplicationController
     end
   end
 
-  # DELETE /posts/1
   def destroy
     @post.destroy
     redirect_to posts_url, notice: 'Post was successfully destroyed.'
@@ -53,8 +46,8 @@ class PostsController < ApplicationController
     @post = Post.includes(:comments).find(params[:id])
   end
 
-    # Only allow a trusted parameter "white list" through.
-    def post_params
-      params.require(:post).permit(:user_id, :title, :body)
-    end
+  private def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
 end
